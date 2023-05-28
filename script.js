8da5fd5c3b417ae8766cbc69027419621f387c3a
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-28 12:26:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let inputs = document.getElementsByClassName("code");
+
+inputs.forEach((input, index)=>{
+  input.addEventListener('input', (ele)=>{
+    const value = ele.target.value;
+    if (value !== '') {
+      if (index < inputs.length - 1) {
+          inputs[index + 1].focus();
+      }
+  } else {
+      if (index > 0) {
+          inputs[index - 1].focus();
+      }
+  }
+});
+
+input.addEventListener('keydown', (e) => {
+  if (e.key === 'Backspace' && index > 0 && input.value === '') {
+      inputs[index - 1].focus();
+  }
+});
+});
