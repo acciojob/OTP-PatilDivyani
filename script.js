12671c67dde65ecb178c84b1ff9c5cd38b7d11c0
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-29 13:55:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 //your JS code here. If required.
-let inputs = document.getElementsByClassName('code');
+
+let inputs = document.getElementsByClassName("code");
 
 for(let i = 0; i< inputs.length; i++){
+	
   inputs[i].addEventListener("keyup", (event) => {
-    let cellInput = event.key.charCodeAt(0);
+   
     let currElement = event.target;
-    if(event.key === "Backspace"){
-      
-     let previousElement =  currElement.previouElementSibling
-     if(previousElement)
-     previousElement.focus();
+	  
+    if(event.key === "Backspace"){ 
+     let previousElement =  currElement.previouElementSibling;
+     if(previousElement) {previousElement.focus();}
     }
+	  return;
     }
+
+	 let cellInput = event.key.charCodeAt(0);						 
     if(48<=cellInput && cellInput<=57) {
       //"0" ascii 48
        let nextElement = currElement.nextElementSibling;
-        if(nextElement)
-       nextElement.focus();
+        if(nextElement) {nextElement.focus();}
       }
     else{
       event.target.value = "";
@@ -25,34 +28,3 @@ for(let i = 0; i< inputs.length; i++){
 }
 
 
-
-/*
-inputs.forEach((input, index) => {
-  input.addEventListener('input', (e) => {
-      const value = e.target.value;
-
-      if (value !== '') {
-        if (index < inputs.length - 1) {
-            inputs[index + 1].focus();
-        }
-    } else {
-        if (index > 0) {
-            inputs[index - 1].focus();
-        }
-    }
-});
-
-input.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace' && index > 0 && input.value === '') {
-        inputs[index - 1].focus();
-    }
-});
-});
-
-function getOTPValue() {
-  let otp = '';
-  inputs.forEach(input => {
-      otp += input.value;
-  });
-  return otp;
-}*/
